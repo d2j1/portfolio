3d1697584b1edd37c62917909401979a29ab6551
Dhananjay <dhananjayjadhav2151@gmail.com>
2024-06-19 19:20:22 +0530

    Services updated

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,55 @@
+"use client";
+import { FaHtml5, FaCss3, FaJs, FaJava, FaReact } from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiPostman,
+  SiApachemaven,
+  SiJunit5,
+  SiGithub,
+  SiGit,
+  SiMysql,
+  SiSpringboot,
+  SiHibernate,
+  SiSolid,
+  SiJira,
+  SiSplunk,
+  SiJenkins,
+} from "react-icons/si";
+
+const about = {
+  title: "About Me",
+  description:
+    "This is the descriptions, please update it once your design is completed.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Dhananay Jadhav",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+91) 8830449186",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "2.4+ Years",
+    },
+
+    {
+      fieldName: "Nationality",
+      fieldValue: "Indian",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "dhananjayjadhav2151@gmail.com",
+    },
+    {
+      fieldName: "Language",
+      fieldValue: "English, Marathi, Hindi",
+    },
+  ],
+};
+
 const Resume = () => {
   return <div>resume page</div>;
 };
