a1d02942b6ad3ce2063d6cfbac2ad605755b35ec
Dhananjay <dhananjayjadhav2151@gmail.com>
2024-06-25 19:15:46 +0530

    project description changed

--- app/work/Values.jsx
+++ app/work/Values.jsx
@@ -32,7 +32,7 @@ export const projects = [
     category: "Productivity App, Frontend",
     title: "Investment Calculator",
     description:
-      "This project is helpful to calculate the return of the investment after few years",
+      "The Investment Calculator is a dynamic web application designed to help users estimate the future value of their investments. Built with React.js for a responsive and interactive user experience, the app leverages Tailwind CSS to ensure a sleek and modern design. Users can input initial investment amounts, expected annual interest rates, and investment duration to calculate potential returns.",
     stack: [
       {
         name: "Html 5",
@@ -48,15 +48,16 @@ export const projects = [
       },
     ],
     image: path + "/assets/investmentCalculator.png",
-    live: "https://d2j1.github.io/pp-investment-calculator",
+    live: "",
     github: "https://github.com/d2j1/pp-investment-calculator",
   },
 
   {
     num: "03",
     category: "Game, Frontend",
-    title: "Tik Tak Toe",
-    description: "This is the tik tok toe game",
+    title: "Tic Tac Toe",
+    description:
+      "This Tic Tac Toe project is a classic two-player game built using React.js, providing a seamless and interactive user experience. Tailwind CSS is utilized to design a modern, responsive, and aesthetically pleasing interface. Players can alternate turns, marking Xs and Os on a 3x3 grid until one achieves three in a row or the game ends in a draw. The project demonstrates effective state management and component-based architecture in React. It also highlights the use of utility-first CSS through Tailwind for rapid UI development and customization.",
     stack: [
       {
         name: "Html 5",
@@ -73,6 +74,6 @@ export const projects = [
     ],
     image: path + "/assets/tiktactoe.png",
     live: "",
-    github: "",
+    github: "https://github.com/d2j1/pp-tic-tac-toe",
   },
 ];
